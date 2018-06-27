2fdb8d4e681aab75d7b1acb9d65ae3d4380131c6
Rizwan Mansuri <mr.mansuri@gmail.com>
2018-06-27 21:54:07 +0100

    Used decorator for registering adminmodel

--- djangocms_internalsearch/admin.py
+++ djangocms_internalsearch/admin.py
@@ -1,13 +1,7 @@
 from django.contrib import admin
-from django.db import models
-from cms.extensions import PageExtensionAdmin
-from cms.extensions.extension_pool import extension_pool
-from cms.extensions.models import PageExtension
 from .models import InternalSearch
 
 
+@admin.register(InternalSearch)
 class InternalSearchAdmin(admin.ModelAdmin):
     pass
-
-
-admin.site.register(InternalSearch, InternalSearchAdmin)
