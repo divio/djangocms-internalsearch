f8af8c02b577a5aa96c2a801159f1387551cb561
Rizwan Mansuri <mr.mansuri@gmail.com>
2018-06-28 09:58:12 +0100

    Added blank admin and models.py and commnet on cms toolbar

--- djangocms_internalsearch/cms_toolbars.py
+++ djangocms_internalsearch/cms_toolbars.py
@@ -1,11 +1,18 @@
-from django.core.urlresolvers import reverse
 from django.utils.translation import ugettext_lazy as _
 from cms.toolbar_pool import toolbar_pool
 from cms.toolbar_base import CMSToolbar
+try:
+    from django.urls import reverse
+except ImportError:
+    from django.core.urlresolvers import reverse
+
 
 @toolbar_pool.register
 class InternalSearchToolbar(CMSToolbar):
+    """
+    Adding button to CMS toolbar to access plugin admin area
+    """
 
-    #TODO: make url dynamic
     def populate(self):
-        menu = self.toolbar.add_button('Internal search', '/admin/djangocms_internalsearch')
+        menu = self.toolbar.add_button('Internal search',
+                                       '/admin/djangocms_internalsearch')
