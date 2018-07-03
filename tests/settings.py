51206916626226c22435a99b56e393f0c9999a52
Rizwan Mansuri <mr.mansuri@gmail.com>
2018-07-03 08:31:53 +0100

    Removed duplicate settting flag

--- tests/settings.py
+++ tests/settings.py
@@ -1,12 +1,3 @@
-HELPER_SETTINGS = {
-    'INSTALLED_APPS': [
-        'djangocms_internalsearch',
-        # 'djangocms_internalsearch.test_utils.project.app_with_cms_config'
-
-    ],
-}
-
-
 def run():
     from djangocms_helper import runner
     runner.cms('djangocms_internalsearch',  extra_args=[])
