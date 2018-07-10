a20e96bf9b8da4d90ee39f031cccced6c6498291
Rizwan Mansuri <mr.mansuri@gmail.com>
2018-07-10 14:58:33 +0100

    Added verbose name for test apps

--- djangocms_internalsearch/test_utils/app_1/apps.py
+++ djangocms_internalsearch/test_utils/app_1/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class App1Config(AppConfig):
     name = 'djangocms_internalsearch.test_utils.app_1'
     label = 'app_1'
+    verbose_name = "django app with cms_config for integration test"
