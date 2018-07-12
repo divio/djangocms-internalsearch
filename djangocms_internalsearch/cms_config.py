504ce15f6f6c973f4a564b3f6a9e6665ba584e4d
Sravan Jinna <sravan.jinna@fil.com>
2018-07-12 09:30:57 +0100

    class factory code with riz to review

--- djangocms_internalsearch/cms_config.py
+++ djangocms_internalsearch/cms_config.py
@@ -3,6 +3,7 @@ from collections import Iterable
 from django.core.exceptions import ImproperlyConfigured
 
 from cms.app_base import CMSAppExtension
+from .search_util import create_search_index_for_haystack
 
 
 class InternalSearchCMSExtension(CMSAppExtension):
@@ -16,6 +17,7 @@ class InternalSearchCMSExtension(CMSAppExtension):
             app_models = getattr(cms_config, 'search_models')
             if isinstance(app_models, Iterable):
                 self.internalsearch_models.extend(app_models)
+                create_search_index_for_haystack(app_models)
             else:
                 raise ImproperlyConfigured(
                     "models configuration for internalsearch must be a Iterable object")
--- djangocms_internalsearch/search_util.py
+++ djangocms_internalsearch/search_util.py
@@ -5,19 +5,51 @@ from haystack import indexes
 from django.core.exceptions import ImproperlyConfigured
 
 from djangocms_internalsearch import cms_config
+from cms.models import CMSPlugin, Page
+from cms.models import pluginmodel
 
 
-def create_search_index_for_haystack(self):
+def create_search_index_for_haystack(model_list):
 
-    model_list = cms_config.InternalSearchCMSExtension().internalsearch_models
+    '''
+    #model_list = cms_config.InternalSearchCMSExtension().internalsearch_models
 
     if not model_list:
         raise ImproperlyConfigured(
             "internal search expect models, got none")
     else:
         for model in model_list:
+            attr_list = [f.name for f in model._meta.get_fields()]
+            print('attr_list %s' % attÎr_list)
             class_created = class_factory(model + 'SearchIndex')
             class_created.get_class_model = partial(get_class_model(model), class_created)
+    '''
+    #model_list_local = ['CMSPlugin']
+
+    #the_module = __import__('cms.models', globals(), locals(), [model_list_local[0]])
+
+    #model = model_list_local[0]
+    class_created = class_factory('PageSearchIndex')
+    instance_of_factory_class = class_created()
+    instance_of_factory_class.text = indexes.CharField(document=True)
+
+    class_created.get_model = partial(get_model('Page'), class_created)
+    model_obj = class_created.get_model()
+
+
+    #---working---
+    #dup_created = duplicate_class_factory('Meta', class_created)
+    #instance_of_dup_class = dup_created()
+    #instance_of_dup_class.model = model_obj
+    #dup_created.model = model_obj
+    #---working---
+
+    #model_fields_list = [f.name for f in model_obj._meta.get_fields()]
+    #print('list of fields %s' % model_fields_list)
+    #model_field_type = model_obj._meta.get_field(model_fields_list[-1]).get_internal_type()
+    #print('model field type %s' % model_field_type)
+    #class_created.publisher_is_draft = indexes.CharField(model_attr='publisher_is_draft')
+    #class_created.
 
 
 class BaseClass(indexes.SearchIndex, indexes.Indexable):
@@ -31,9 +63,24 @@ def class_factory(name, BaseClass=BaseClass):
 
     new_class = type(name, (BaseClass,), {"__init__": __init__})
 
+
     return new_class
 
 
-def get_class_model(model_name):
+class DuplicateBaseClass():
+    def __init__(self, class_type):
+        self._type = class_type
+
+
+def duplicate_class_factory(name, parent,  DuplicateBaseClass=DuplicateBaseClass):
+    def __init__(self):
+        DuplicateBaseClass.__init__(self, name)
+
+    dup_class = type(name, (DuplicateBaseClass, parent), {"__init__": __init__})
+    #dup_class.model = None
+    return dup_class
+
+
+def get_model(model_name):
     return getattr(sys.modules[__name__], model_name)
 
