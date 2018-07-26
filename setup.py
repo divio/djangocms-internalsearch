299eed0f6327d2588d717cf785ded2d791313454
Rizwan <rizwan@webbyfox.com>
2018-07-26 11:26:05 +0100

    Added haystack in Requirement

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ import djangocms_internalsearch
 INSTALL_REQUIREMENTS = [
     'Django>=1.11,<2.0',
     'django-cms>=3.5.0',
+    'django-haystack>=2.7.0',
 ]
 
 
--- tests/test_create_indexes.py
+++ tests/test_create_indexes.py
@@ -1,17 +1,13 @@
-from django.apps import apps
-from django.test import TestCase
-
 from cms import app_registration
 from cms.test_utils.testcases import CMSTestCase
-from cms.utils.setup import setup_cms_apps
 
-from djangocms_internalsearch.search_indexes import (
-    create_indexes,
-)
+from djangocms_internalsearch.search_indexes import create_indexes
 from djangocms_internalsearch.test_utils.app_2.cms_config import (
     TestModel1Config,
     TestModel2Config,
 )
+
+from .utils import TestCase
 
 
 class ClassFactoryUnitTestCase(TestCase):
@@ -33,20 +29,19 @@ class ClassFactoryUnitTestCase(TestCase):
         self.assertEqual(TestModel1Index.__name__, 'TestModel1Index')
 
 
-class InternalSearchIntegrationTestCase(CMSTestCase):
+class InternalSearchIntegrationTestCase(CMSTestCase, TestCase):
 
     def setUp(self):
         app_registration.get_cms_extension_apps.cache_clear()
         app_registration.get_cms_config_apps.cache_clear()
 
     def test_config_with_two_apps(self):
-        setup_cms_apps()
-        internalsearch_config = apps.get_app_config('djangocms_internalsearch')
-        registered_model = internalsearch_config.cms_extension.internalsearch_models
-
-        create_indexes(registered_model)
-
-        try:
-            from djangocms_internalsearch.search_indexes import TestModel1Index
-        except ImportError:
-            self.fail("Cannot import the created class")
+        """
+        App initialise should have created search indexes
+        """
+        with self.assertNotRaises(AttributeError):
+            from djangocms_internalsearch import search_indexes
+            search_indexes.TestModel1Index
+            search_indexes.TestModel2Index
+            search_indexes.TestModel3Index
+            search_indexes.TestModel4Index
