155f660993cb830d79f44c7b1f0578d1f5bbf42d
Rizwan <rizwan@webbyfox.com>
2018-07-27 08:59:19 +0100

    Added new fields on list display

--- djangocms_internalsearch/cms_config.py
+++ djangocms_internalsearch/cms_config.py
@@ -40,10 +40,10 @@ class PageModelConfig:
     fields = [
         'page_title', 'slug', 'site_id', 'site_name', 'language',
         'cmsplugin_type_array', 'text', 'created_by', 'changed_by',
-        'creation_date', 'changed_date', 'html_source',
+        'version_status', 'creation_date', 'changed_date', 'html_source',
     ]
 
-    list_display = ('page_title', 'language',)
+    list_display = ('page_title', 'language', 'version_status', 'changed_by')
     list_filter = ('language', 'site_name', 'changed_by',)
     auto_create_index_fields = False
 
