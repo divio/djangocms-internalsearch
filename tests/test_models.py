5ac1a1fae053a4b81fd82d9dc25f624a6427826f
Dino Livanos <dinolivanos+git@gmail.com>
2018-07-27 17:24:16 +0100

    fix tox errors

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,6 +1,7 @@
-from django.test import TestCase
-from django.contrib.contenttypes.models import ContentType
 from django.contrib.auth.models import User
+from django.contrib.contenttypes.models import ContentType
+from django.test import TestCase
+
 from djangocms_internalsearch.models import Query
 
 
