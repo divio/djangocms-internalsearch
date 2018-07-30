37d06966a7015739f7b5160d5245b7d685b46ecb
Sravan Jinna <sravan.jinna@fil.com>
2018-07-30 14:48:30 +0100

    correction in Indentation

--- djangocms_internalsearch/search_indexes.py
+++ djangocms_internalsearch/search_indexes.py
@@ -24,6 +24,10 @@ def class_factory(name, model):
         return model
 
     index_class_dict = {"text": text, "get_model": get_model}
-    new_class = type(name, (indexes.SearchIndex, indexes.Indexable), index_class_dict)
+    new_class = type(
+        name,
+        (indexes.SearchIndex, indexes.Indexable),
+        index_class_dict
+    )
 
     return new_class
