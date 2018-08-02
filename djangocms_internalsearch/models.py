cc16412948467bfb9da623f936108383fc0bbb5a
Sravan Jinna <sravan.jinna@fil.com>
2018-08-02 14:14:10 +0100

    cleaned the Query model and acted on paulo's comments

--- djangocms_internalsearch/models.py
+++ djangocms_internalsearch/models.py
@@ -1,12 +1,7 @@
-from django.contrib.auth.models import User
+from django.conf import settings
 from django.db import models
 
 
 class Query(models.Model):
-    query_time = models.DateTimeField(auto_now_add=True)
-    query_string = models.TextField()
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-
-    def save(self, *args, **kwargs):
-        # TODO validate/clean before saving
-        super().save(*args, **kwargs)
+    query_string = models.CharField(max_length=255, db_index=True)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
