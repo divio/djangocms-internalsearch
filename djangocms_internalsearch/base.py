c6ffd8439365897e5ce98900453b73ce8a18728d
Paulo Alvarado <commonzenpython@gmail.com>
2018-08-23 10:08:35 -0400

    renamed

--- djangocms_internalsearch/base.py
+++ djangocms_internalsearch/base.py
@@ -6,7 +6,7 @@ class BaseSearchConfig(indexes.SearchIndex, indexes.Indexable):
     Base config class to provide list of attributes that sub class must provide
     """
     text = indexes.CharField(document=True, use_template=False)
-    ng_text = indexes.NgramField(document=False, use_template=False)
+    text_ngram = indexes.NgramField(document=False, use_template=False)
 
     @property
     def model(self):
@@ -22,5 +22,5 @@ class BaseSearchConfig(indexes.SearchIndex, indexes.Indexable):
     def prepare_text(self, obj):
         raise NotImplementedError("Config class must provide prepare_text method for index")
 
-    def prepare_ng_text(self, obj):
+    def prepare_text_ngram(self, obj):
         return self.prepare_text(obj)
