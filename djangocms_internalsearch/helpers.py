6bb17a59447cc55bf4e99451fd50f5bc43d9342a
Sravan Jinna <jinnashravan@gmail.com>
2018-08-28 17:04:40 +0100

    new function to reduce clutter in helpers

--- djangocms_internalsearch/helpers.py
+++ djangocms_internalsearch/helpers.py
@@ -4,7 +4,7 @@ from haystack import connections
 def save_to_index(sender, operation, request, token, **kwargs):
 
     from django.utils.translation import get_language_from_request
-    from cms.operations import DELETE_PAGE, DELETE_PLUGIN
+
     from cms.models.titlemodels import Title
     from filer.models.filemodels import File
     from filer.models.imagemodels import Image
@@ -12,39 +12,40 @@ def save_to_index(sender, operation, request, token, **kwargs):
     if 'obj' in kwargs:
         index = connections["default"].get_unified_index().get_index(Title)
         obj = kwargs['obj'].get_title_obj(get_language_from_request(request, check_path=False))
-        if operation == DELETE_PAGE:
-            index.remove_object(obj)
-        else:
-            index.update_object(obj)
+        update_or_remove_object(index, obj, operation)
+
     if 'new_plugin' in kwargs:
         index = connections["default"].get_unified_index().get_index(Title)
         plc = kwargs['new_plugin'].placeholder
         obj = plc.page.get_title_obj(get_language_from_request(request, check_path=False))
         index.update_object(obj)
+
     if 'plugin' in kwargs:
         plugin_type = kwargs['plugin'].plugin_type
         if plugin_type == 'TextPlugin':
             index = connections["default"].get_unified_index().get_index(Title)
 
             obj = kwargs['placeholder'].page.get_title_obj(get_language_from_request(request, check_path=False))
-            if operation is DELETE_PLUGIN:
-                index.remove_object(obj)
-            else:
-                index.update_object(obj)
+            update_or_remove_object(index, obj, operation)
+
         if plugin_type == 'FilePlugin':
             index = connections["default"].get_unified_index().get_index(File)
 
             obj = kwargs['plugin']
-            if operation is DELETE_PLUGIN:
-                index.remove_object(obj.file_src.file.instance)
-            else:
-                index.update_object(obj.file_src.file.instance)
+            update_or_remove_object(index, obj.file_src.file.instance, operation)
 
         if plugin_type == 'PicturePlugin':
             index = connections["default"].get_unified_index().get_index(Image)
 
             obj = kwargs['plugin']
-            if operation is DELETE_PLUGIN:
-                index.remove_object(obj.picture)
-            else:
-                index.update_object(obj.picture)
+            update_or_remove_object(index, obj.picture, operation)
+
+
+def update_or_remove_object(index, obj, operation):
+
+    from cms.operations import DELETE_PAGE, DELETE_PLUGIN
+
+    if operation is DELETE_PLUGIN or operation is DELETE_PAGE:
+        index.remove_object(obj)
+    else:
+        index.update_object(obj)
