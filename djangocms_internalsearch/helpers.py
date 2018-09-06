1810bcd5c53337ff4c30acbc39ec8a16b22925d6
Krzysztof Socha <k.socha@merixstudio.com>
2018-09-06 17:30:31 +0200

    Rewrite updating index

--- djangocms_internalsearch/helpers.py
+++ djangocms_internalsearch/helpers.py
@@ -1,36 +1,64 @@
-from cms.operations import DELETE_PAGE, DELETE_PLUGIN
+from cms.operations import (
+    ADD_PAGE_TRANSLATION,
+    ADD_PLUGIN,
+    CHANGE_PAGE_TRANSLATION,
+    CHANGE_PLUGIN,
+    DELETE_PAGE,
+    DELETE_PAGE_TRANSLATION,
+    DELETE_PLUGIN,
+    MOVE_PLUGIN,
+)
+
+from django.utils.translation import get_language_from_request
 
 from haystack import connections
-from haystack.exceptions import NotHandled
+
+
+def delete_page(index, request, **kwargs):
+    obj = kwargs['obj'].get_title_obj(get_language_from_request(request))
+    index.remove_object(obj)
+
+
+def update_page_content(index, request, **kwargs):
+    obj = kwargs['obj'].get_title_obj(get_language_from_request(request))
+    index.update_object(obj)
+
+
+def delete_page_content(index, request, **kwargs):
+    obj = kwargs['obj'].get_title_obj(get_language_from_request(request))
+    index.remove_object(obj)
+
+
+def update_plugin(index, request, **kwargs):
+    index.update_object(kwargs['placeholder'].source)
+
+
+def delete_plugin(index, request, **kwargs):
+    index.remove_object(kwargs['placeholder'].source)
+
+
+def move_plugin(index, request, **kwargs):
+    index.update_object(kwargs['source_placeholder'].source)
+    index.update_object(kwargs['target_placeholder'].source)
 
 
 def save_to_index(sender, operation, request, token, **kwargs):
-    from django.utils.translation import get_language_from_request
+    from cms.models import Title
 
-    from cms.models.titlemodels import Title
+    index = connections["default"].get_unified_index().get_index(Title)
 
-    if 'obj' in kwargs:
-        index = connections["default"].get_unified_index().get_index(Title)
-        obj = kwargs['obj'].get_title_obj(get_language_from_request(request))
-        save_object(index, obj, operation)
+    operation_actions = {
+        DELETE_PAGE: delete_page,
+        ADD_PAGE_TRANSLATION: update_page_content,
+        CHANGE_PAGE_TRANSLATION: update_page_content,
+        DELETE_PAGE_TRANSLATION: delete_page_content,
+        ADD_PLUGIN: update_plugin,
+        CHANGE_PLUGIN: update_plugin,
+        DELETE_PLUGIN: delete_plugin,
+        MOVE_PLUGIN: move_plugin,
+    }
 
-    if 'new_plugin' in kwargs:
-        index = connections["default"].get_unified_index().get_index(Title)
-        plc = kwargs['new_plugin'].placeholder
-        obj = plc.page.get_title_obj(get_language_from_request(request))
-        index.update_object(obj)
+    if operation not in operation_actions:
+        return
 
-    if 'plugin' in kwargs:
-        try:
-            plugin = kwargs['plugin']
-            index = connections["default"].get_unified_index().get_index(type(plugin))
-            save_object(index, obj, operation)
-        except NotHandled:
-            pass
-
-
-def save_object(index, obj, operation):
-    if operation in [DELETE_PLUGIN, DELETE_PAGE]:
-        index.remove_object(obj)
-    else:
-        index.update_object(obj)
+    operation_actions[operation](index, request, **kwargs)
