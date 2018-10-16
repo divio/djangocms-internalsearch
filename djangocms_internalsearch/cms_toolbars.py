2eaddd60c4b26635dade69d26f05f4a982479dec
Matus Moravcik <matus.moravcik@gmail.com>
2018-10-16 15:00:09 +0100

    Use reverse instead of hardcoded URL

--- djangocms_internalsearch/cms_toolbars.py
+++ djangocms_internalsearch/cms_toolbars.py
@@ -3,6 +3,8 @@ from django.utils.translation import ugettext_lazy as _
 from cms.toolbar_base import CMSToolbar
 from cms.toolbar_pool import toolbar_pool
 
+from djangocms_internalsearch.models import InternalSearchProxy
+
 
 try:
     from django.urls import reverse
@@ -15,10 +17,11 @@ class InternalSearchToolbar(CMSToolbar):
     """
     Adding button to CMS toolbar to access plugin admin area
     """
-
     def populate(self):
         self.toolbar.add_sideframe_button(
             _('Internal search'),
-            # TODO: make url dynamic
-            '/admin/djangocms_internalsearch/internalsearchproxy'
+            reverse('admin:{app_label}_{model_name}_changelist'.format(
+                app_label=InternalSearchProxy._meta.app_label,
+                model_name=InternalSearchProxy._meta.model_name,
+            )),
         )
